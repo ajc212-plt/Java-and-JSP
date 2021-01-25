04a448b11adccf6213337d08900742d7132e49c3
AJC <ajc212@naver.com>
2021-01-25 21:47:54 +0900

    Second Commit

--- src/main/java/UserService/Test.java
+++ src/main/java/UserService/Test.java
@@ -3,5 +3,6 @@ package UserService;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello! github");
+		System.out.println("second Practice Upload Github");
 	}
 }
