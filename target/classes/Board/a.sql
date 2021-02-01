902a2d4873bc59948ba4c4a5ca743ad27db86194
AJC <ajc212@naver.com>
2021-02-01 21:03:12 +0900

    Study aop

--- target/classes/Board/a.sql
+++ target/classes/Board/a.sql
@@ -7,3 +7,18 @@ insert into AhnJongCan values('¾ÈÁ¾Âù');
 select * from AHNJONGCAN
 
 select * from board
+
+delete from board where seq = 1
+delete from board where seq = 2
+delete from board where seq = 3;
+delete from board where seq = 4;
+delete from board where seq = 5;
+delete from board where seq = 6;
+delete from board where seq = 7;
+delete from board where seq = 8;
+
+create table springtest(
+	id varchar2(20),
+	name varchar2(20),
+	joindate date default sysdate
+);
